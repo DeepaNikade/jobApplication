2800805601f6180377875b1a3b45e1b1be1b772b
Deepa <nikadedeepa01@gmail.com>
2024-05-21 17:30:12 +0530

    changes in router

--- routes/job.js
+++ routes/job.js
@@ -4,8 +4,8 @@ const jobController = require("../controllers/job");
 const router = express.Router();
 
 router.post("/api/jobs", jobController.createJob);
-router.get("/api/jobs", jobController.listJob);  
-router.put("/api/jobs/:id", jobController.editJob); 
-router.delete("/api/jobs/:id", jobController.deleteJob);
+router.get("/api/jobs", jobController.listJob);
+router.put("/api/:jobId", jobController.editJob);
+router.delete("/api/:jobId", jobController.deleteJob);
 
 module.exports = router;
